2076ed078867a58d599905766587614aca4bf9d5
roydenwa <royden-wagner@outlook.com>
2022-03-30 22:36:38 +0200

    Add a subclass version of the model.

--- src/efficientcellseg/model_architecture.py
+++ src/efficientcellseg/model_architecture.py
@@ -2,8 +2,87 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers, models
 from tensorflow.keras.applications import EfficientNetB5
+
+
+class EfficientCellSeg(models.Model):
+    def __init__(self, input_shape):
+        super().__init__(self)
+        self._layers = {}
+        self.input_layer = layers.Input(input_shape, name="input_resized")
+        self.backbone = EfficientNetB5(
+            input_tensor=self.input_layer,
+            weights="imagenet",
+            include_top=False
+        )
+        self.encoder = models.Model(
+            inputs=self.backbone.input,
+            outputs=self.backbone.get_layer("block6a_expand_activation").output
+        )
+        self.skip_connections = [
+            "input_resized",
+            "block1a_activation",
+            "block3a_expand_activation",
+            "block4a_expand_activation"
+        ]
+
+    def get(self, name, ctor, *args, **kwargs):
+        """Helper func to skip initializing layers in the init method."""
+        if name not in self._layers:
+            self._layers[name] = ctor(*args, **kwargs)
+        return self._layers[name]
+
+    def call(self, img):
+        x = self.encoder(img)
+        x = self.get("up1", layers.UpSampling2D, interpolation="bilinear")(x)
+        skip1 = self.encoder.get_layer(self.skip_connections[-1]).output
+        x = self.get("concat1", layers.Concatenate)([x, skip1])
+        x = self.get("conv1.1", layers.Conv2D, filters=64, kernel_size=3, padding="same")(x)
+        x = self.get("bn1.1", layers.BatchNormalization)(x)
+        x = self.get("relu1.1", layers.Activation, "relu")(x)
+
+        x = self.get("conv1.2", layers.Conv2D, filters=64, kernel_size=3, padding="same")(x)
+        x = self.get("bn1.2", layers.BatchNormalization)(x)
+        x = self.get("relu1.2", layers.Activation, "relu")(x)
+
+        x = self.get("up2", layers.UpSampling2D, interpolation="bilinear")(x)
+        skip2 = self.encoder.get_layer(self.skip_connections[2]).output
+        x = self.get("concat2", layers.Concatenate)([x, skip2])
+        x = self.get("conv2.1", layers.Conv2D, filters=48, kernel_size=3, padding="same")(x)
+        x = self.get("bn2.1", layers.BatchNormalization)(x)
+        x = self.get("relu2.1", layers.Activation, "relu")(x)
+
+        x = self.get("conv2.2", layers.Conv2D, filters=48, kernel_size=3, padding="same")(x)
+        x = self.get("bn2.2", layers.BatchNormalization)(x)
+        x = self.get("relu2.2", layers.Activation, "relu")(x)
+
+        x = self.get("up3", layers.UpSampling2D, interpolation="bilinear")(x)
+        skip3 = self.encoder.get_layer(self.skip_connections[1]).output
+        x = self.get("concat3", layers.Concatenate)([x, skip3])
+        x = self.get("conv3.1", layers.Conv2D, filters=48, kernel_size=3, padding="same")(x)
+        x = self.get("bn3.1", layers.BatchNormalization)(x)
+        x = self.get("relu3.1", layers.Activation, "relu")(x)
+
+        x = self.get("conv3.2", layers.Conv2D, filters=48, kernel_size=3, padding="same")(x)
+        x = self.get("bn3.2", layers.BatchNormalization)(x)
+        x = self.get("relu3.2", layers.Activation, "relu")(x)
+
+        x = self.get("up4", layers.UpSampling2D, interpolation="bilinear")(x)
+        skip4 = self.encoder.get_layer(self.skip_connections[0]).output
+        x = self.get("concat4", layers.Concatenate)([x, skip4])
+        x = self.get("conv4.1", layers.Conv2D, filters=48, kernel_size=3, padding="same")(x)
+        x = self.get("bn4.1", layers.BatchNormalization)(x)
+        x = self.get("relu4.1", layers.Activation, "relu")(x)
+
+        x = self.get("conv4.2", layers.Conv2D, filters=48, kernel_size=3, padding="same")(x)
+        x = self.get("bn4.2", layers.BatchNormalization)(x)
+        x = self.get("relu4.2", layers.Activation, "relu")(x)
+
+        x = self.get("conv_last", layers.Conv2D, filters=1, kernel_size=1, padding="same")(x)
+        x = self.get("sigmoid", layers.Activation, "sigmoid")(x)
+
+        return x
 
 
 def create_efficient_cell_seg(img_h: int = None, img_w: int = None, inner_h: int = 384,
